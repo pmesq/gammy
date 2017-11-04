ec35a4feb8af1be45ff74cd57b14140d063f8f06
Pedro César (Alface) <pedrocesar1410@gmail.com>
2017-11-04 19:19:37 -0200

    Update conquistas.js

--- javascript/conquistas.js
+++ javascript/conquistas.js
@@ -1,17 +1,41 @@
 let unlockedEl = document.querySelector('#unlocked'),
   lockedEl = document.querySelector('#locked');
 
-if(localStorage.getItem('conqJDV1'))
+// Conquistas do Jogo da Velha
+
+if(JSON.parse(localStorage.getItem('conqJDV1')))
   unlockedEl.innerHTML += '<h3>Eu começo primeiro!</h3><p>Jogar Jogo da Velha 5 vezes.</p>';
 else
   lockedEl.innerHTML += '<h3>Eu começo primeiro!</h3><p>Jogar Jogo da Velha 5 vezes.</p>';
 
-if(localStorage.getItem('conqJDV2'))
+if(JSON.parse(localStorage.getItem('conqJDV2')))
   unlockedEl.innerHTML += '<h3>XXX</h3><p>Jogar Jogo da Velha 25 vezes.</p>';
 else
   lockedEl.innerHTML += '<h3>XXX</h3><p>Jogar Jogo da Velha 25 vezes.</p>';
 
-if(localStorage.getItem('conqJDV3'))
+if(JSON.parse(localStorage.getItem('conqJDV3')))
   unlockedEl.innerHTML += '<h3>Jogo do Mestre</h3><p>Jogar Jogo da Velha 100 vezes.</p>';
 else
   lockedEl.innerHTML += '<h3>Jogo do Mestre</h3><p>Jogar Jogo da Velha 100 vezes.</p>';
+
+// Conquistas do Alcap
+
+if(JSON.parse(localStorage.getItem('conqALCAP1')))
+  unlockedEl.innerHTML += '<h3>Você me achou!</h3><p>Achar a alface escondida no fundo</p>';
+else
+  lockedEl.innerHTML += '<h3>Você me achou!</h3><p>Achar a alface escondida no fundo</p>';
+
+if(JSON.parse(localStorage.getItem('conqALCAP2')))
+  unlockedEl.innerHTML += '<h3>Patrão</h3><p>Contratar seu primeiro agricultor</p>';
+else
+  lockedEl.innerHTML += '<h3>Patrão</h3><p>Contratar seu primeiro agricultor</p>';
+
+if(JSON.parse(localStorage.getItem('conqALCAP3')))
+  unlockedEl.innerHTML += '<h3>Big Plantation</h3><p>Possuir 10000 AlfaceCoins</p>';
+else
+  lockedEl.innerHTML += '<h3>Big Plantation</h3><p>Possuir 10000 AlfaceCoins</p>';
+
+if(JSON.parse(localStorage.getItem('conqALCAP4')))
+  unlockedEl.innerHTML += '<h3>Alfacilionário</h3><p>Possuir 1000000 de AlfaceCoins</p>';
+else
+  lockedEl.innerHTML += '<h3>Alfacilionário</h3><p>Possuir 1000000 de AlfaceCoins</p>';
