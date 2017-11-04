4e7608014b5179f1146b8e89813d5b73bbf84031
Pedro César (Alface) <pedrocesar1410@gmail.com>
2017-11-04 19:34:43 -0200

    Update conquistas.js

--- javascript/conquistas.js
+++ javascript/conquistas.js
@@ -4,38 +4,38 @@ let unlockedEl = document.querySelector('#unlocked'),
 // Conquistas do Jogo da Velha
 
 if(JSON.parse(localStorage.getItem('conqJDV1')))
-  unlockedEl.innerHTML += '<h3>Eu começo primeiro!</h3><p>Jogar Jogo da Velha 5 vezes.</p>';
+  unlockedEl.innerHTML += '<div class="conq jdv"><h3>Eu começo primeiro!</h3><p>Jogar Jogo da Velha 5 vezes.</p></div>';
 else
-  lockedEl.innerHTML += '<h3>Eu começo primeiro!</h3><p>Jogar Jogo da Velha 5 vezes.</p>';
+  lockedEl.innerHTML += '<div class="conq jdv"><h3>Eu começo primeiro!</h3><p>Jogar Jogo da Velha 5 vezes.</p></div>';
 
 if(JSON.parse(localStorage.getItem('conqJDV2')))
-  unlockedEl.innerHTML += '<h3>XXX</h3><p>Jogar Jogo da Velha 25 vezes.</p>';
+  unlockedEl.innerHTML += '<div class="conq jdv"><h3>XXX</h3><p>Jogar Jogo da Velha 25 vezes.</p></div>';
 else
-  lockedEl.innerHTML += '<h3>XXX</h3><p>Jogar Jogo da Velha 25 vezes.</p>';
+  lockedEl.innerHTML += '<div class="conq jdv"><h3>XXX</h3><p>Jogar Jogo da Velha 25 vezes.</p></div>';
 
 if(JSON.parse(localStorage.getItem('conqJDV3')))
-  unlockedEl.innerHTML += '<h3>Jogo do Mestre</h3><p>Jogar Jogo da Velha 100 vezes.</p>';
+  unlockedEl.innerHTML += '<div class="conq jdv"><h3>Jogo do Mestre</h3><p>Jogar Jogo da Velha 100 vezes.</p></div>';
 else
-  lockedEl.innerHTML += '<h3>Jogo do Mestre</h3><p>Jogar Jogo da Velha 100 vezes.</p>';
+  lockedEl.innerHTML += '<div class="conq jdv"><h3>Jogo do Mestre</h3><p>Jogar Jogo da Velha 100 vezes.</p></div>';
 
 // Conquistas do Alcap
 
 if(JSON.parse(localStorage.getItem('conqALCAP1')))
-  unlockedEl.innerHTML += '<h3>Você me achou!</h3><p>Achar a alface escondida no fundo</p>';
+  unlockedEl.innerHTML += '<div class="conq alcap"><h3>Você me achou!</h3><p>Achar a alface escondida no fundo</p></div>';
 else
-  lockedEl.innerHTML += '<h3>Você me achou!</h3><p>Achar a alface escondida no fundo</p>';
+  lockedEl.innerHTML += '<div class="conq alcap"><h3>Você me achou!</h3><p>Achar a alface escondida no fundo</p></div>';
 
 if(JSON.parse(localStorage.getItem('conqALCAP2')))
-  unlockedEl.innerHTML += '<h3>Patrão</h3><p>Contratar seu primeiro agricultor</p>';
+  unlockedEl.innerHTML += '<div class="conq alcap"><h3>Patrão</h3><p>Contratar seu primeiro agricultor</p></div>';
 else
-  lockedEl.innerHTML += '<h3>Patrão</h3><p>Contratar seu primeiro agricultor</p>';
+  lockedEl.innerHTML += '<div class="conq alcap"><h3>Patrão</h3><p>Contratar seu primeiro agricultor</p></div>';
 
 if(JSON.parse(localStorage.getItem('conqALCAP3')))
-  unlockedEl.innerHTML += '<h3>Big Plantation</h3><p>Possuir 10000 AlfaceCoins</p>';
+  unlockedEl.innerHTML += '<div class="conq alcap"><h3>Big Plantation</h3><p>Possuir 10000 AlfaceCoins</p></div>';
 else
-  lockedEl.innerHTML += '<h3>Big Plantation</h3><p>Possuir 10000 AlfaceCoins</p>';
+  lockedEl.innerHTML += '<div class="conq alcap"><h3>Big Plantation</h3><p>Possuir 10000 AlfaceCoins</p></div>';
 
 if(JSON.parse(localStorage.getItem('conqALCAP4')))
-  unlockedEl.innerHTML += '<h3>Alfacilionário</h3><p>Possuir 1000000 de AlfaceCoins</p>';
+  unlockedEl.innerHTML += '<div class="conq alcap"><h3>Alfacilionário</h3><p>Possuir 1000000 de AlfaceCoins</p></div>';
 else
-  lockedEl.innerHTML += '<h3>Alfacilionário</h3><p>Possuir 1000000 de AlfaceCoins</p>';
+  lockedEl.innerHTML += '<div class="conq alcap"><h3>Alfacilionário</h3><p>Possuir 1000000 de AlfaceCoins</p></div>';
