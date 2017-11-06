9f1d5bd29e82d8cc3eccd2193e12149698780dfe
Pedro César (Alface) <pedrocesar1410@gmail.com>
2017-11-06 11:00:56 -0200

    Add files via upload

--- javascript/editar-perfil.js
+++ javascript/editar-perfil.js
@@ -1,7 +1,9 @@
 let editNomeEl = document.querySelector('#edit-nome'),
+  editDescEl = document.querySelector('#edit-desc'),
   confirmEl = document.querySelector('#confirmar'),
   cancelEl = document.querySelector('#cancelar');
-editNomeEl.value = localStorage.getItem('nome');
+
 confirmEl.addEventListener('click', function() {
   localStorage.setItem('nome', editNomeEl.value);
+  localStorage.setItem('desc', editDescEl.value);
 });
--- javascript/perfil.js
+++ javascript/perfil.js
@@ -1,5 +1,6 @@
 let divEditNomeEl = document.querySelector('#editar-nome'),
   nomeEl = document.querySelector('#p-nome'),
+  descEl = document.querySelector('#p-desc'),
   conquistasEl = document.querySelector('#p-conquistas'),
   conquistas = 0;
 
@@ -7,6 +8,12 @@ if(localStorage.getItem('nome') == null)
   localStorage.setItem('nome', 'Você');
 else
   nomeEl.innerHTML = localStorage.getItem('nome');
+
+if(localStorage.getItem('desc') == null)
+  localStorage.setItem('desc', 'Sua descrição');
+else
+  descEl.innerHTML = localStorage.getItem('desc');
+
 if(localStorage.getItem('conquistas') == null)
   localStorage.setItem('conquistas', '0');
 else
