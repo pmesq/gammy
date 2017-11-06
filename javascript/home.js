5d1375389ac438adf4ad21905e269865e52d49e9
SamuelSimonetti <samuelribeirosimonetti10@gmail.com>
2017-11-06 21:08:18 -0200

    Add files via upload

--- javascript/home.js
+++ javascript/home.js
@@ -3,7 +3,7 @@ let infoEl = document.querySelector('#info'),
 
 botaoInfoEl.addEventListener('click', function() {
   infoEl.classList.add('info');
-  infoEl.innerHTML = '<button id="botao-fechar-info">X</button><br><p>Gammy é uma rede de jogos, que possui aproximadamente 2 jogos. O site possui um sistema de conquistas, que incentiva mais ainda o usuário a jogar. O site foi desenvolvido por Pedro César (Alface), Sammuel Reis (Sammy) e Samuel Simonetti (Sabonete).</p>';
+  infoEl.innerHTML = '<button id="botao-fechar-info">X</button><br><p>Gammy é uma rede de jogos, que possui em sua atual versão de testes aproximadamente 2 jogos. O site possui um sistema de conquistas, que incentiva mais ainda o usuário a jogar. O site foi desenvolvido por Pedro César (Alface), Sammuel Reis (Sammy) e Samuel Simonetti (Sabonete).<br>Bem vindo ao gammy!</p>';
   let botaoFecharInfoEl = document.querySelector('#botao-fechar-info');
   botaoFecharInfoEl.addEventListener('click', function() {
     infoEl.innerHTML = '';
